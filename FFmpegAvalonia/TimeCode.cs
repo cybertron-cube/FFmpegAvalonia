b9c7d36bb6aa567cbafdc5e08cde1331e4757b48
Blitznir <54194687+Blitznir@users.noreply.github.com>
2023-04-24 06:03:45 -0400

    Added extra timecode methods, started some comments, and changed the timecode regex to be stricter

--- FFmpegAvalonia/TimeCode.cs
+++ FFmpegAvalonia/TimeCode.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using AvaloniaEdit;
 using ExtensionMethods;
 
 namespace FFmpegAvalonia
 {
+    /// <summary>
+    /// Represents a timecode consisting of hours, minutes, seconds, milliseconds
+    /// </summary>
     public class TimeCode
     {
-        private static readonly Regex _timeCodeRegex = new("(?<hours>\\d\\d):(?<minutes>\\d\\d):(?<seconds>\\d\\d).(?<milliseconds>\\d+)");
+        public static readonly Regex TimeCodeRegex = new("^(?<hours>\\d\\d):(?<minutes>\\d\\d):(?<seconds>\\d\\d).(?<milliseconds>\\d\\d\\d)$");
         private short _hours;
+        /// <summary>
+        /// Hours unit of the timecode
+        /// </summary>
         public short Hours
         {
             get { return _hours; }
@@ -27,6 +28,9 @@ namespace FFmpegAvalonia
             }
         }
         private short _minutes;
+        /// <summary>
+        /// Minutes unit of the timecode
+        /// </summary>
         public short Minutes
         {
             get { return _minutes; }
@@ -41,6 +45,9 @@ namespace FFmpegAvalonia
             }
         }
         private short _seconds;
+        /// <summary>
+        /// Seconds unit of the timecode
+        /// </summary>
         public short Seconds
         {
             get { return _seconds; }
@@ -55,6 +62,9 @@ namespace FFmpegAvalonia
             }
         }
         private short _milliseconds;
+        /// <summary>
+        /// Milliseconds unit of the timecode
+        /// </summary>
         public short Milliseconds
         {
             get { return _milliseconds; }
@@ -69,8 +79,14 @@ namespace FFmpegAvalonia
             }
         }
         private int _value;
+        /// <summary>
+        /// Integer of the entire timecode used to compare to other timecodes
+        /// </summary>
         public int Value { get { return _value; } }
         private string _formattedString;
+        /// <summary>
+        /// Timecode represented in a properly formatted string (hh:mm:ss:msmsms)
+        /// </summary>
         public string FormattedString { get { return _formattedString; } }
         public TimeCode(short hours, short minutes, short seconds, short milliseconds)
         {
@@ -85,9 +101,15 @@ namespace FFmpegAvalonia
                 PadTimeCodeUnit(milliseconds, 3));
             _value = ToInt(_formattedString);
         }
+        /// <summary>
+        /// Creates a new <see cref="TimeCode"/> object from a string
+        /// </summary>
+        /// <param name="text">A string that contains a timecode represented in hh:mm:ss:msmsms</param>
+        /// <returns>A <see cref="TimeCode"/> object from a valid string</returns>
+        /// <exception cref="FormatException">Timecode string was not represented in hh:mm:ss:msmsms</exception>
         public static TimeCode Parse(string text)
         {
-            var match = _timeCodeRegex.Match(text);
+            Match match = TimeCodeRegex.Match(text);
             if (match.Success)
             {
                 TimeCode timeCode = new(
@@ -99,17 +121,119 @@ namespace FFmpegAvalonia
             }
             else throw new FormatException("Input string was not in a correct format.");
         }
-        public static string TimeSpan(TimeCode timeCodeOne, TimeCode timeCodeTwo)
+        /// <summary>
+        /// Creates a new <see cref="TimeCode"/> object from a string/>
+        /// </summary>
+        /// <param name="text">A string that contains a timecode represented in hh:mm:ss:msmsms</param>
+        /// <param name="timeCode">The <see cref="TimeCode"/> representation of the text</param>
+        /// <returns>A boolean indicating whether the string is able to be parsed</returns>
+        public static bool TryParse(string text, out TimeCode? timeCode)
         {
-            throw new NotImplementedException();
+            Match match = TimeCodeRegex.Match(text);
+            if (match.Success)
+            {
+                timeCode = new(
+                    Int16.Parse(match.Groups["hours"].Value),
+                    Int16.Parse(match.Groups["minutes"].Value),
+                    Int16.Parse(match.Groups["seconds"].Value),
+                    Int16.Parse(match.Groups["milliseconds"].Value));
+                return true;
+            }
+            else
+            {
+                timeCode = null;
+                return false;
+            }
         }
+        /// <summary>
+        /// Calculates the difference between two <see cref="TimeCode"/> objects
+        /// </summary>
+        /// <param name="timeCode1">First <see cref="TimeCode"/> object</param>
+        /// <param name="timeCode2">Second <see cref="TimeCode"/> object</param>
+        /// <returns>A <see cref="TimeCode"/> object that represents the timespan between the given timecodes</returns>
+        public static TimeCode TimeSpan(TimeCode timeCode1, TimeCode timeCode2)
+        {
+            int msDiff;
+            int sDiff;
+            int mDiff;
+            int hDiff;
+            if (timeCode1.Value > timeCode2.Value)
+            {
+                msDiff = timeCode1.Milliseconds - timeCode2.Milliseconds;
+                sDiff = timeCode1.Seconds - timeCode2.Seconds;
+                mDiff = timeCode1.Minutes - timeCode2.Minutes;
+                hDiff = timeCode1.Hours - timeCode2.Hours;
+            }
+            else
+            {
+                msDiff = timeCode2.Milliseconds - timeCode1.Milliseconds;
+                sDiff = timeCode2.Seconds - timeCode1.Seconds;
+                mDiff = timeCode2.Minutes - timeCode1.Minutes;
+                hDiff = timeCode2.Hours - timeCode1.Hours;
+            }
+            if (msDiff < 0)
+            {
+                msDiff += 1000;
+                sDiff -= 1;
+            }
+            if (sDiff < 0)
+            {
+                sDiff += 60;
+                mDiff -= 1;
+            }
+            if (mDiff < 0)
+            {
+                mDiff += 60;
+                hDiff -= 1;
+            }
+            return new TimeCode((short)hDiff, (short)mDiff, (short)sDiff, (short)msDiff);
+        }
+        /// <summary>
+        /// Converts a timecode string represented in hh:mm:ss:msmsms to the literal value
+        /// </summary>
+        /// <param name="str">Timecode string represented in hh:mm:ss:msmsms</param>
+        /// <returns>The string without colons and period converted to an integer</returns>
+        /// <exception cref="FormatException">Timecode string was not represented in hh:mm:ss:msmsms</exception>
         public static int ToInt(string str)
         {
-            return Int32.Parse(str.Split(':', '.').Combine());
+            Match match = TimeCodeRegex.Match(str);
+            if (match.Success)
+            {
+                return Int32.Parse(str.Split(':', '.').Combine());
+            }
+            else throw new FormatException("Input string was not in a correct format.");
+        }
+        public static int? TryParseToInt(string str)
+        {
+            Match match = TimeCodeRegex.Match(str);
+            if (match.Success)
+            {
+                return Int32.Parse(str.Split(':', '.').Combine());
+            }
+            else return null;
+        }
+        public static bool TryParseToInt(string str, out int result)
+        {
+            Match match = TimeCodeRegex.Match(str);
+            if (match.Success)
+            {
+                result = Int32.Parse(str.Split(':', '.').Combine());
+                return true;
+            }
+            else
+            {
+                result = 0;
+                return false;
+            }
         }
         public static double ToDouble(string str)
         {
-            return double.Parse(str.Split(':', '.').Combine());
+            var match = TimeCodeRegex.Match(str);
+            if (match.Success)
+            {
+                return double.Parse(str.Split(':', '.').Combine());
+            }
+            else throw new FormatException("Input string was not in a correct format.");
         }
         public static string PadTimeCodeUnit(int unit, int places = 2)
         {
