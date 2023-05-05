acbcff537145a4b3155d4e768279dcac11b50735
Blitznir <54194687+Blitznir@users.noreply.github.com>
2023-05-05 00:22:27 -0400

    Clean up using statements

--- FFmpegAvalonia/AppSettingsX/Settings.cs
+++ FFmpegAvalonia/AppSettingsX/Settings.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FFmpegAvalonia.AppSettingsX;
 
-namespace FFmpegAvalonia.AppSettingsX
-{
     internal class Settings : PropertyReflection
     {
         public string FFmpegPath { get; set; } = "";
         public string FrameCountMethod { get; set; } = "GetFrameCountApproximate";
         public string UpdateTarget { get; set; } = "release";
         public bool AutoOverwriteCheck { get; set; }
-        public bool CopySourceCheck { get; set; }
-    }
 }
